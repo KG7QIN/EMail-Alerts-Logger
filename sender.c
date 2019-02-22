2652e9d7c5bcd44ce7fbe100ad0d2c083e5aaa53
Stacy Olivas <olivas@eurisko.ws>
2019-02-22 09:15:07 -0800

    Missing a file

--- sender.c
+++ sender.c
@@ -1,31 +1,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <syslog.h>
 
 #include "session.h"
 
 #include "sender.h"
 
+char *fixstr(char *tmp)
+{
+	int i, len = strlen(tmp);
+	char *dst = strdup(tmp);
+	char c;
+
+	for (i=0; i<len; i++)
+	{
+		c = tmp[i];
+		if (c < 32 || c > 127) c = 32;
+		dst[i] = c;
+	}
+	return dst;
+}
+
 void* sender_worker(void* data) {
-	printf("sender: starting work\n");
 
 	struct message_container* message = (struct message_container*) data;
-	printf("sender: sending message\n");
-	printf("sender: FROM: %s\n", message->mail_from);
-	printf("sender: TO: %s\n", message->rcpt_to);
-	printf("sender: BODY:\n");
+
+	openlog (SYSLOG_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL7);
+
+	char *msg = (char*) malloc(sizeof(char) * 1024);
+
+	
+
 	if (message->body)
-		printf("%s", message->body);
+	{
+		snprintf(msg, 1024, "MESSAGE: %s - %s", fixstr(message->mail_from), fixstr(message->body));
+	}
 	else
-		printf("No body.\n");
-	printf("\n");
+	{
+		snprintf(msg, 1024, "MESSAGE: %s - Nothing received.", fixstr(message->mail_from));
+	}
+
+	syslog (LOG_INFO, "%s", msg);
+	closelog();
 
 	// cleanup
+	free(msg);
 	free(message->mail_from);
 	free(message->rcpt_to);
 	free(message->body);
 	free(message);
-	printf("sender: finished\n");
 	return NULL;
+
 }
 
